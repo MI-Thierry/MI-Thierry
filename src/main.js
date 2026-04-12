9a81492201b17adc681a406e08e07b2e5e3cfb3a
Thierry I. Muhirwa <mithierry1010@gmail.com>
2026-04-12 11:09:51 +0200

    Clean up mobile navbar toggle code
    Removed commented-out code for mobile navbar visibility.

--- src/main.js
+++ src/main.js
@@ -20,7 +20,6 @@ window.openMobileNavbar = () => {
   const mobileNavbar = document.getElementById("mobile-navbar");
   const overlay = document.getElementById("overlay");
 
-  //mobileNavbar.classList.remove("hidden");
   mobileNavbar.classList.remove("-translate-y-60");
   mobileNavbar.classList.add("translate-0");
 
@@ -30,7 +29,6 @@ window.openMobileNavbar = () => {
   overlay.addEventListener("click", () => {
     mobileNavbar.classList.remove("translate-0");
     mobileNavbar.classList.add("-translate-y-60");
-    //mobileNavbar.classList.add("hidden");
 
     overlay.classList.remove("pointer-events-auto");
     overlay.classList.add("pointer-events-none");
